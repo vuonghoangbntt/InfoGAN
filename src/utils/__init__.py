6ebe0b7cba9ad3ff1bf423ab842dcc3c64158330
Vuong Hoang <vuonghoangbntt@gmail.com>
2022-04-20 16:41:03 +0700

    debug: fix NLL Gaussian Loss function

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,2 @@
-from .model import weights_init_normal, sample_latent
+from .model import weights_init_normal, sample_latent, set_seed
 from .logger import init_logger, reset_logger
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,5 +1,14 @@
 import torch
 import numpy as np
+import random
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
 
 def weights_init_normal(m):
     classname = m.__class__.__name__
